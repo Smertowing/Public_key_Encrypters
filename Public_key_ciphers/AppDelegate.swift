23ce032c34b7b40aeda404619c2bc3be742fc0b0
Kiryl <Kiryl.co@gmail.com>
2018-10-07 17:19:22 +0300

    Initial Commit

---
+++ Public_key_ciphers/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Public_key_ciphers
+//
+//  Created by Kiryl Holubeu on 10/7/18.
+//  Copyright © 2018 Kiryl Holubeu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
